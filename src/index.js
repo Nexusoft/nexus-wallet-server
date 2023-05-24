bf23114162ccc400f930059b4c2deca6b5d3b442
Krysto <quy.p.hoang@gmail.com>
2023-05-24 11:17:03 +0700

    migrate to coingecko API

--- src/index.js
+++ src/index.js
@@ -2,61 +2,36 @@ import express from 'express';
 import cors from 'cors';
 import axios from 'axios';
 
-const apiKey = process.env.API_KEY;
-const batch1 =
-  'BTC,USD,EUR,AUD,BRL,GBP,CAD,CLP,CNY,CZK,HKD,ILS,JPY,KRW,MYR,MXN';
-const batch2 =
-  'NZD,PKR,RUB,SAR,SGD,ZAR,CHF,TWD,AED,INR,PLN,VND,THB,MMK,IDR,PHP';
-const getUrl = (tsyms) =>
-  `https://min-api.cryptocompare.com/data/pricemultifull?api_key=${apiKey}&fsyms=NXS,BTC&tsyms=${tsyms}`;
+// const apiKey = process.env.API_KEY;
+const currencies =
+  'BTC,USD,EUR,AUD,BRL,GBP,CAD,CLP,CNY,CZK,HKD,ILS,JPY,KRW,MYR,MXN,NZD,PKR,RUB,SAR,SGD,ZAR,CHF,TWD,AED,INR,PLN,VND,THB,MMK,IDR,PHP';
+const url = `https://api.coingecko.com/api/v3/simple/price?ids=nexus&vs_currencies=${currencies}&include_24hr_change=true`;
 
 let marketData = null;
+let lastFetched = null;
 let error = null;
 
-if (!apiKey) {
-  console.log('API_KEY environment variable not found!');
-}
+const validTime = 15 * 60 * 1000; // data expires after 15 minutes
+const isDataValid = () =>
+  !!marketData && !!lastFetched && Date.now() - lastFetched <= validTime;
+
+// if (!apiKey) {
+//   console.log('API_KEY environment variable not found!');
+// }
 
 async function fetchMarketData() {
   try {
-    const [prices1, prices2] = await Promise.all([
-      axios(getUrl(batch1)),
-      axios(getUrl(batch2)),
-    ]);
-    console.log('prices1', prices1);
-    console.log('prices2', prices2);
+    const prices = await axios(url);
 
-    if (!prices1?.data || !prices2?.data) {
-      marketData = null;
-      error = { message: 'Invalid data returned from cryptocompare' };
+    if (!prices?.data?.nexus) {
+      error = new Error('Invalid data returned from coingecko');
       return;
     }
 
-    marketData = {
-      RAW: {
-        NXS: {
-          ...prices1.data.RAW.NXS,
-          ...prices2.data.RAW.NXS,
-        },
-        BTC: {
-          ...prices1.data.RAW.BTC,
-          ...prices2.data.RAW.BTC,
-        },
-      },
-      DISPLAY: {
-        NXS: {
-          ...prices1.data.DISPLAY.NXS,
-          ...prices2.data.DISPLAY.NXS,
-        },
-        BTC: {
-          ...prices1.data.DISPLAY.BTC,
-          ...prices2.data.DISPLAY.BTC,
-        },
-      },
-    };
+    marketData = prices.data.nexus;
+    lastFetched = Date.now();
     error = null;
   } catch (err) {
-    marketData = null;
     error = err;
     console.error('Failed to fetch market data');
     console.error(err);
@@ -70,21 +45,20 @@ function servePriceData(app) {
     })
   );
 
-  // backward compatibility
-  app.get('/displaydata', (req, res) => {
-    if (marketData) {
-      res.json(marketData);
-    } else {
-      res.status(500).json({ error });
-    }
-  });
+  // // backward compatibility
+  // app.get('/displaydata', (req, res) => {
+  //   if (marketData) {
+  //     res.json(marketData);
+  //   } else {
+  //     res.status(500).json({ error });
+  //   }
+  // });
 
   app.get('/market-data', (req, res) => {
-    if (marketData) {
-      const baseCurrency = req.query?.base_currency;
-      const data = marketData.RAW.NXS[baseCurrency];
-      const price = data.PRICE;
-      const changePct24Hr = data.CHANGEPCT24HOUR;
+    if (isDataValid()) {
+      const baseCurrency = req.query?.base_currency?.toLowerCase();
+      const price = marketData[baseCurrency];
+      const changePct24Hr = marketData[`${baseCurrency}_24h_change`];
       res.json({
         price,
         changePct24Hr,
@@ -95,9 +69,9 @@ function servePriceData(app) {
   });
 
   app.get('/market-price', (req, res) => {
-    if (marketData) {
-      const baseCurrency = req.query?.base_currency;
-      const price = marketData.RAW.NXS[baseCurrency].PRICE;
+    if (isDataValid()) {
+      const baseCurrency = req.query?.base_currency?.toLowerCase();
+      const price = marketData[baseCurrency];
       res.json({
         price,
       });
